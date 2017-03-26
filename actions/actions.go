17c40b83aeafbd2b6164b518f20432ff6e55e3d6
David Terry <davidterry@posteo.de>
2017-03-27 01:01:54 +0200

    formatting: sort functions in actions.go alphabetically

--- actions/actions.go
+++ actions/actions.go
@@ -9,6 +9,15 @@ import (
 	"strings"
 )
 
+// Attach spawns a bash shell in the container with given name
+func Attach(containerName string) {
+	cmd := exec.Command("docker", "exec", "-i", "-t", containerName, "/bin/bash")
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Run()
+}
+
 // BuildImage builds Dockerfile specified in the config and returns the resulting Image ID
 func BuildImage(dockerFileDirectory string) string {
 	stdoutStderr := docker("build", "--quiet", dockerFileDirectory)
@@ -20,23 +29,16 @@ func BuildImage(dockerFileDirectory string) string {
 	return imageID
 }
 
+// Destroy the container with the given name
+func Destroy(containerName string) {
+	docker("rm", "--force", containerName)
+	fmt.Println("Destroyed container with name:", containerName)
+}
+
 // StartBackgroundContainer brings up a container with the given imageID and volume mappings
 func StartBackgroundContainer(imageID string, name string, volumes map[string]string) {
 	docker("run", "-dti", volumeArgs(volumes), "--name", name, imageID)
 	fmt.Println("started background container withname:", name)
 }
 
-// Attach spawns a bash shell in the container with given name
-func Attach(containerName string) {
-	cmd := exec.Command("docker", "exec", "-i", "-t", containerName, "/bin/bash")
-	cmd.Stdin = os.Stdin
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	cmd.Run()
-}
 
-// Destroy the container with the given name
-func Destroy(containerName string) {
-	docker("rm", "--force", containerName)
-	fmt.Println("Destroyed container with name:", containerName)
-}
