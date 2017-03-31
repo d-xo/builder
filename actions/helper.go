16ed2221e779f95d938e3165002efd29eacc718f
David Terry <davidterry@posteo.de>
2017-04-01 00:57:04 +0200

    add a few initial docker helpers

--- actions/helper.go
+++ actions/helper.go
@@ -1,11 +1,47 @@
 package actions
 
 import (
+	"context"
+	"errors"
 	"fmt"
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/client"
 	"os"
 	"os/exec"
 	"strings"
 )
+
+func dockerClient() *client.Client {
+	cli, err := client.NewEnvClient()
+	if err != nil {
+		panic(err)
+	}
+	return cli
+}
+
+func isContainerPresent(containerName string) bool {
+	containers, err := dockerClient().ContainerList(context.Background(), types.ContainerListOptions{})
+	if err != nil {
+		panic(err)
+	}
+
+	return hasContainerWithName(containerName, containers)
+}
+
+func container(containerName string) (types.Container, error) {
+	return types.Container{}, errors.New("Container not present")
+}
+
+func hasContainerWithName(containerName string, containers []types.Container) bool {
+	for _, container := range containers {
+		for _, name := range container.Names {
+			if name == containerName {
+				return true
+			}
+		}
+	}
+	return false
+}
 
 func docker(args ...string) string {
 	cmd := exec.Command("docker", args...)
