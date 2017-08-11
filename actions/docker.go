39a8c2f87ccb2edd012b2949fffef2e26a474ffb
David Terry <davidterry@posteo.de>
2017-08-11 22:04:14 +0200

    small refactoring in actions layer
    dockerClient() returns a client and a context
    split out starting a background container into a few smaller functions

--- actions/docker.go
+++ actions/docker.go
@@ -1,6 +1,7 @@
 package actions
 
 import (
+	"context"
 	"fmt"
 	"os"
 	"os/exec"
@@ -9,12 +10,13 @@ import (
 	"github.com/docker/docker/client"
 )
 
-func dockerClient() *client.Client {
+func dockerClient() (*client.Client, context.Context) {
+	ctx := context.Background()
 	cli, err := client.NewEnvClient()
 	if err != nil {
 		panic(err)
 	}
-	return cli
+	return cli, ctx
 }
 
 func containerWithNameExists(containerName string, containers []types.Container) bool {
