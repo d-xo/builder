e4e84a428d8ccdd88bd880dea4891bb8ab2ba03b
David Terry <davidterry@posteo.de>
2017-08-16 21:25:16 +0200

    remove superflous comments

--- actions/actions.go
+++ actions/actions.go
@@ -24,7 +24,6 @@ func Attach(containerName string) {
 
 // BuildImage builds Dockerfile specified in the config and returns the resulting Image ID
 // run build twice, once to build the image (with stdout), and once to get the image ID
-// this is UGLY
 func BuildImage(dockerFileDirectory string) string {
 	cmd := exec.Command("docker", "build", dockerFileDirectory)
 	cmd.Stdin = os.Stdin
